5dda62925cac0b8b20e800ef32632359e5a7cce2
HarryNguyen2023 <gianguyenlamvinh2002@gmail.com>
2023-08-10 10:01:10 +0700

    Add source code for UART library and adjust the main application code for the project

--- PIC16F877A_HCSR04_capture.c
+++ PIC16F877A_HCSR04_capture.c
@@ -1,41 +1,55 @@
 #include "config.h"
 #include "PIC16F877A_HCSR04.h"
 #include "PIC16F877A_timer0.h"
+#include "PIC16F877A_UART.h"
+#include <stdlib.h>
+#include <string.h>
 
 // Globale variable
-uint8_t count  = 0;
+uint8_t count = 0;
+
+// UART send buffer
+char send_buff[45];
 
 // Function to handle interrupt
 void __interrupt() ISR(void)
 {
     // Timer 0 overflow interrupt
-    if(TMR0IF)
+    if (TMR0IF)
     {
         TMR0 = 6;
-        if(count++ == 100)
+        if (count++ == 100)
         {
             count = 0;
             hcsr04Trigger();
+            double dist = getDistance();
+            if (dist > 0)
+            {
+                memset(send_buff, '\0', sizeof(send_buff));
+                sprintf(send_buff, "Distance to obstacle: %.3f cm\n", dist);
+                UARTsendString(send_buff);
+            }
         }
         TMR0IF = 0;
     }
     // Input capture 1 interrupt
-    if(CCP1IF)
+    if (CCP1IF)
     {
         hcsr04Distance();
         CCP1IF = 0;
     }
 }
 
-void main(void) {
+void main(void)
+{
     // Initiate Timer 0 timer mode
     TMR0 = 6;
     timer0TimerInit(TIMER0_DIV_16);
-    
+
     // Initiate the HCSR04 sensor interface
     hcsr04Init();
-    
-    while(1)
+
+    while (1)
     {
     }
     return;
--- PIC16F877A_UART.h
+++ PIC16F877A_UART.h
@@ -3,10 +3,23 @@
 
 #include <xc.h>
 
-void UARTInit(void);
+// Define the baud rate for the UART module
+#define BAUD_RATE 57600
+
+// Variable to keep track the string reception via UART
+uint16_t uart_str_idx = 0;
+
+// UART receive buffer
+extern uint16_t str_len;
+char
+
+    void
+    UARTTransInit(void);
+void UARTRcvInit(void);
+void UARTTransRcvInit(void);
 void UARTsendChar(char c);
 void UARTsendString(char *str);
 char UARTrcvChar(void);
-char* UARTrcvString(uint16_t length);
+char *UARTrcvString(uint16_t length);
 
 #endif
